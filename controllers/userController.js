4405c04701f5fde985d726924c27c0773ee74d86
Ranidu <ranidukarunanayake@gmail.com>
2025-06-18 19:58:55 +0530

    jwt key hiding

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,9 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config()
+
 export function saveUser(req,res){
 
     if(req.body.role == "admin"){
@@ -63,7 +66,7 @@ export function loginUser(req,res){
                     isEmailVerified: user.isEmailVerified,
                 }
             
-                const token = jwt.sign(userData,"random456")
+                const token = jwt.sign(userData,process.env.JWT_KEY)
 
                 res.json({
                     message: "Login successful",
